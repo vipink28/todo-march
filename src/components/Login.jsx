a2413ea54c3feb5871ef090319f3333310580a2f
vipink28 <vipinkalyan28@gmail.com>
2023-04-05 13:14:15 +0530

    register user added with json server

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 function Login(props) {
     return (
-        <div>
-            Login
-        </div>
+        <form>
+            <div className='mb-3'>
+                <label className='form-label'>Email</label>
+                <input type="text" name='email' className='form-control' />
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'>Password</label>
+                <input type="password" name='password' className='form-control' />
+            </div>
+            <button className='btn btn-primary'>Login</button>
+        </form>
     );
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register(props) {
+
+    const [formData, setFormData]= useState(null);
+
+    const handleChange = (e)=>{
+        const { name, value } = e.target;
+        setFormData((prev)=>({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const onsubmit=async(e)=>{
+        const obj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+        e.preventDefault();
+        // api call
+        const response = await fetch(`http://localhost:5000/users`, obj);
+
+        if(response.ok){
+            alert("User Regsitered");
+        }else{
+            alert("something went wrong");
+        }
+    }
+
+
+
+
     return (
-        <div>
-            Register
-        </div>
+        <form>
+            <div className='mb-3'>
+                <label className='form-label'>Name</label>
+                <input type="text" name='username' className='form-control' onChange={handleChange} />
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'>Email</label>
+                <input type="text" name='email' className='form-control' onChange={handleChange} />
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'>Password</label>
+                <input type="password" name='password' className='form-control' onChange={handleChange}/>
+            </div>
+            <button className='btn btn-primary' onClick={onsubmit}>Register</button>
+        </form>
     );
 }
 
