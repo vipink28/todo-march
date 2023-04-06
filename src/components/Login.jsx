3ab944093f455a4bbc9bc7303c373a78d5e9e29e
vipink28 <vipinkalyan28@gmail.com>
2023-04-06 12:55:16 +0530

    confirm user before registration.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(props) {
+
+    const [formData, setFormData]= useState(null);
+    const [message, setMessage] = useState("");
+
+    const handleChange = (e)=>{
+        const { name, value } = e.target;
+        setFormData((prev)=>({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const onSubmit=async(e)=>{
+        e.preventDefault();
+        const response = await fetch(`http://localhost:5000/users?email=${formData.email}&password=${formData.password}`, {method: "GET"});
+        if(response.ok){
+            const checkUser =await response.json();
+            if(checkUser.length > 0){
+                setMessage("Logged in successfully");
+            }
+            else{
+                setMessage("Email/Password mismatch");
+            }
+        }
+        else{
+            setMessage("Please try again");
+        }
+    }
+
     return (
         <form>
             <div className='mb-3'>
                 <label className='form-label'>Email</label>
-                <input type="text" name='email' className='form-control' />
+                <input type="text" name='email' className='form-control' onChange={handleChange} />
             </div>
             <div className='mb-3'>
                 <label className='form-label'>Password</label>
-                <input type="password" name='password' className='form-control' />
+                <input type="password" name='password' className='form-control' onChange={handleChange}/>
             </div>
-            <button className='btn btn-primary'>Login</button>
+            <p>{message}</p>
+            <button className='btn btn-primary' onClick={onSubmit}>Login</button>
         </form>
     );
 }
